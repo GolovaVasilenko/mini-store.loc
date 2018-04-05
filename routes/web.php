92b7529600d0eb5a093850bc8465ce086458b7d3
GolovaVasilenko <vasilenko75@gmail.com>
2018-04-05 16:23:41 +0300

    add style admin panel

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,7 @@ Route::get('/', 'PageController@home')->name('home');
 
 Auth::routes();
 
-//Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => 'admin', 'middleware' => ['web', 'auth']], function() {
-    Route::get('/', 'Admin\DashboardController@index')->name('admin');
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'auth'], function() {
+    Route::get('/', 'DashboardController@index')->name('admin');
 });
