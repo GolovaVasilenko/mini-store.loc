eb8027c11444338a1e61f5ae3603e623c5123d10
GolovaVasilenko <vasilenko75@gmail.com>
2018-04-05 12:48:35 +0300

    change Admin controller

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,4 +7,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => ['web', 'auth']], function() {
+    Route::get('/', 'Admin\DashboardController@index')->name('admin');
+});
