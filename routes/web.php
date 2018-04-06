ad79e68b069b7ae7070f4e48a076c8b09c390faa
GolovaVasilenko <vasilenko75@gmail.com>
2018-04-06 14:16:49 +0300

    add debagbar

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', 'PageController@home')->name('home');
 
 Auth::routes();
 
-
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'auth'], function() {
-    Route::get('/', 'DashboardController@index')->name('admin');
+    Route::get('/', 'DashboardController@index')->name('dashboard');
 });
