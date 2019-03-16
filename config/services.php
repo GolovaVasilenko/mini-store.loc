104b5f2ecd4939c7294580d5faf080dbd3f67428
GolovaVasilenko <vasilenko75@gmail.com>
2019-03-16 15:36:18 +0200

    added DB service

--- config/services.php
+++ config/services.php
@@ -4,5 +4,6 @@ return [
     Core\Services\Request\Provider::class,
     Core\Services\Response\Provider::class,
     Core\Services\Router\Provider::class,
+    Core\Services\DB\Provider::class,
     Core\Services\View\Provider::class,
 ];
